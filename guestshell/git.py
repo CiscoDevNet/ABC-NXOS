2f1c58400344064f11f9716914de4b20f6f3d33b
andubiel <andubiel@cisco.com>
2021-02-12 18:45:04 -0500

    Add files via upload

--- guestshell/git.py
+++ guestshell/git.py
@@ -1,1 +1,48 @@
+#!/usr/bin/python
 
+import os
+import subprocess
+from subprocess import call
+from cisco import system
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+CFGDIR = os.path.join(BASEDIR, "config-backup")
+HOSTNAME = system.System().get_hostname()
+
+if not os.path.isdir(os.path.join(CFGDIR, HOSTNAME)):
+    os.mkdir(os.path.join(CFGDIR, HOSTNAME))
+
+os.chdir(CFGDIR)
+
+call(
+    [
+        "cp",
+        "/bootflash/running.latest",
+        "{}/{}/running.latest".format(CFGDIR, HOSTNAME),
+    ]
+)
+call(
+    [
+        "git",
+        "add",
+        "{}/{}/running.latest".format(CFGDIR, HOSTNAME),
+    ]
+)
+call(["git", "commit", "-m", "Configuration change"])
+p = subprocess.Popen(
+    [
+        "chvrf",
+        "management",
+        "git",
+        "push",
+        "http://root:C1sco12345@10.10.20.50/root/config-backup.git",
+        "--all",
+    ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
+out, err = p.communicate()
+
+with open(os.path.join(BASEDIR, "output.log"), "a+") as f:
+    f.write(out)
+    f.write(err)
