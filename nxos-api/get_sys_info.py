098394f8a0afa61b54261ad4c964e5b3b46e48a8
andubiel <andubiel@cisco.com>
2021-02-15 18:01:38 -0500

    Create get_sys_info.py

---
+++ nxos-api/get_sys_info.py
@@ -0,0 +1,34 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+# disable any ssl insecure warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Assign requests.Session instance to session variable
+session = requests.Session()
+
+# Define URL and PAYLOAD variables
+URL = "https://10.10.20.177/api/aaaLogin.json"
+PAYLOAD = {
+          "aaaUser": {
+            "attributes": {
+              "name": "cisco",
+              "pwd": "cisco"
+               }
+            }
+          }
+
+# Obtain an authentication cookie
+session.post(URL,json=PAYLOAD,verify=False)
+
+# Define SYS_URL variable
+SYS_URL = "https://10.10.20.177/api/mo/sys.json"
+
+# Obtain system information by making session.get call
+# then convert it to JSON format then filter to system attributes
+sys_info = session.get(SYS_URL).json()["imdata"][0]["topSystem"]["attributes"]
+
+# Print hostname, serial nmber, uptime and current state information
+# obtained from the NXOSv9k
+print("HOSTNAME:", sys_info["name"])
+print("SERIAL NUMBER:", sys_info["serial"])
+print("UPTIME:", sys_info["systemUpTime"])
